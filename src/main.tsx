4d92c012bdeb9b86637306064188c07da8ac40bc
Breno Henrique <57954979+BrenoHaraujo2503@users.noreply.github.com>
2022-03-16 22:32:37 -0300

    Redirect all routers for LandingPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { AppRoutes } from './routes'
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <AppRoutes />
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../components/Header"
+
+export function LandingPage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
